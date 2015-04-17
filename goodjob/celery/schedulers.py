fc4dbaf0b7506fc71a0ad783ff569671a2658e6d
RussellLuo <luopeng.he@gmail.com>
2015-04-17 11:34:23 +0800

    Remove schedule entries for periodic jobs that have been deleted
    1. use `self.schedule` instead of `self._store['entries']`
    2. use `job.id` as entry name
    3. fix typo

--- goodjob/celery/schedulers.py
+++ goodjob/celery/schedulers.py
@@ -3,6 +3,7 @@
 
 from __future__ import absolute_import
 
+from bson import ObjectId
 from celery.beat import ScheduleEntry, PersistentScheduler
 from celery.schedules import crontab
 
@@ -19,26 +20,39 @@ class DatabaseScheduler(PersistentScheduler):
     """Job scheduler based on database."""
 
     def __init__(self, *args, **kwargs):
-        # force `self._store` to be initialized
+        # force `self.schedule` to be initialized
         kwargs['lazy'] = False
         super(DatabaseScheduler, self).__init__(*args, **kwargs)
 
+        # maximum time to sleep between re-checking the schedule.
         self.max_interval = DEFAULT_MAX_INTERVAL
 
-        # read periodic jobs from database for the first time
+        # schedule all periodic jobs from database for the first time
         self.update_schedule(forced=True)
 
     def update_schedule(self, forced=False):
-        conditon = dict(schedule__ne='')
-        if not forced:
-            conditon.update(has_scheduled=False)
+        """Update schedule entries for periodic jobs."""
 
-        for job in Job.objects(**conditon):
+        condition = dict(schedule__ne='')
+        if not forced:
+            condition.update(has_scheduled=False)
+
+        # remove entries for jobs that have been deleted
+        deleted_entriy_ids = [
+            entry_id
+            for entry_id in self.schedule
+            if Job.objects(id=ObjectId(entry_id)).first() is None
+        ]
+        for entry_id in deleted_entriy_ids:
+            self.remove_entry(entry_id)
+
+        # add/update entries for jobs that have not been scheduled
+        for job in Job.objects(**condition):
             args = job.schedule.split()
             schedule = crontab(*args)
-            self._store['entries'][str(job.id)] = ScheduleEntry(
+            self.schedule[str(job.id)] = ScheduleEntry(
                 app=self.app,
-                name=job.name,
+                name=str(job.id),
                 schedule=schedule,
                 task='goodjob.core_job',
                 args=[job.id],
@@ -48,5 +62,11 @@ class DatabaseScheduler(PersistentScheduler):
             job.save()
 
     def tick(self):
+        """Override to schedule newcome periodic jobs."""
         self.update_schedule()
         return super(DatabaseScheduler, self).tick()
+
+    def remove_entry(self, entry_id):
+        entry_id = str(entry_id)
+        if entry_id in self.schedule:
+            del self.schedule[entry_id]
