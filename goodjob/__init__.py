3c4bfa590df87615ca9a8897d94299b993b2ea5f
RussellLuo <luopeng.he@gmail.com>
2016-03-23 21:00:35 +0800

    Bump version to 0.1.0

--- goodjob/__init__.py
+++ goodjob/__init__.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """A service for executing asynchronous jobs."""
 
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 __author__ = 'RussellLuo'
 __email__ = 'luopeng.he@gmail.com'
 __license__ = 'MIT'
